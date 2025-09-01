37b782dafbb161f99ff2c269ae96cf02ff265870
loganlang393 <langewischl@gmail.com>
2025-09-01 09:48:35 -0400

    Trying to correct final error in python files

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -34,9 +34,7 @@ ALLOWED_HOSTS = [
     + 'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
     + 'proxy.cognitiveclass.ai'
     ]
-CSRF_TRUSTED_ORIGINS = [
-    "https://langewischl-8000.'theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.a"'
-    ]
+CSRF_TRUSTED_ORIGINS = ['https://langewischl-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
