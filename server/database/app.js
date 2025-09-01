fd252fc283b3be08bb79edc488ef228af011f9f7
loganlang393 <langewischl@gmail.com>
2025-09-01 09:59:38 -0400

    Trying to fix the final erros in the javascript files

--- server/database/app.js
+++ server/database/app.js
@@ -1,4 +1,4 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -22,10 +22,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
@@ -95,7 +95,7 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } );
-  let new_id = documents[0]['id']+1;
+  let new_id = documents[0].id+1;
 
   const review = new Reviews({
 		"id": new_id,
